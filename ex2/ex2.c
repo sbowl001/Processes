0480d708b366005076d036a3672aa949e86f1166
Stephanie Bowles <sbowl001@fiu.edu>
2018-11-04 17:07:04 +0800

    ex2

--- ex2/ex2.c
+++ ex2/ex2.c
@@ -5,10 +5,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
+    FILE *fp;
+    fp = fopen("text.txt", "w");   //r+ read + write 
     // Your code here 
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf( stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("child process here %d \n", (int) getpid());
+        char *child_str = "this is child string \n";
+        fwrite(child_str, 1, strlen(child_str), fp);  // 1 is the size of the byte per character in string
+    } else {
+        printf("parent process here\n");
+        char *parent_str = "this is parent string \n";
+        fwrite(parent_str, 1, strlen(parent_str), fp);
+    }
+
+    fclose(fp);
+
     
     return 0;
 }
