dd24e8d15e4406ba1fb9e92a3414ad8f69aa763c
Stephanie Bowles <sbowl001@fiu.edu>
2018-11-04 16:44:47 +0800

    ex1 complete

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -6,9 +6,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main(void)
+int main(int argc, char*argv[])  //why does this also work with void? 
 {
-    // Your code here
+    int x = 100;
+
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if ( rc == 0) {
+        printf("hello, child here (pid: %d) and x is: %d \n" , (int) getpid(), x);
+        x++;
+        printf("child again, x is now: %d \n", x);
+    } else {
+        printf("hello, parent(pid: %d) of child %d \n", (int) getpid(), rc);
+        x--;
+        printf("hello, x of parent is now: %d \n", x);
+    }
 
     return 0;
 }
+
+
+// gcc [NAME OF FILE].c -o [NAME OF EXECUTABLE]
+
+// int main(int argc, char *argv[])
+// {
+//     printf("hello world (pid: %d)\n", (int) getpid());
+//     int rc = fork();
+//     // ------------------------------------------------ child process starts executing here
+//     if (rc < 0) {    // fork failed; exit
+//         fprintf(stderr, "fork failed\n");
+//         exit(1);
+//     } else if (rc == 0) {    // child process satisfies this branch
+//         printf("hello, child here (pid: %d) \n", (int) getpid());
+//     } else {
+//         printf("hello, parent here (pid: %d) of child %d\n", (int) getpid(), rc);
+//     }
+
+//     return 0;
+// }
