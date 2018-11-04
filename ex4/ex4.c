884a83c8f0232aa0b6180cf8869f65988797d488
Stephanie Bowles <sbowl001@fiu.edu>
2018-11-05 00:56:57 +0800

    ex4

--- ex4/ex4.c
+++ ex4/ex4.c
@@ -8,9 +8,27 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 
-int main(void)
+int main(int argc, char* argv[])
 {
     // Your code here    
+    int rc = fork();
+
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("child here \n");
+        // char *args[] = {"ls", "-l", NULL};
+        // execvp(args[0], args);
+
+        // execl("/bin/ls", "ls", "-l", NULL);
+        
+        execlp("ls", "ls", "-l", NULL);
+    } else {
+        int wc = waitpid(rc, NULL, 0);
+        printf("parent here \n");
+    }
+
 
     return 0;
 }
